ee6ac0d7f98244fde034f82784ac9860e35354a7
mxxnseat <dumaesnormalno@gmail.com>
2022-05-22 11:59:08 +0300

    feat(): identified main classes

---
+++ src/classes/index.ts
@@ -0,0 +1,7 @@
+export * from './gameObject.class';
+export * from './map.class';
+export * from './moveable.class';
+export * from './engine.class';
+export * from './collisiable.class';
+export * from './camera.class';
+export * from './attackable.class';
