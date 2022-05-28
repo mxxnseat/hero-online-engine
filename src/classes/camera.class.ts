587638ed7d1aa5f07f6172e01e1e40aa10bf2741
mxxnseat <dumaesnormalno@gmail.com>
2022-05-28 13:44:46 +0300

    chore(): description of main game classes

--- src/classes/camera.class.ts
+++ src/classes/camera.class.ts
@@ -1,3 +1,11 @@
-export class Camera{
-    
+export class Camera implements HeroOnline.interfaces.IGameCamera {
+  public get position() {
+    return this._position;
+  }
+
+  constructor(private _position: HeroOnline.interfaces.IPosition) {}
+
+  move(to: HeroOnline.interfaces.IPosition): void {
+    return void 0;
+  }
 }
