6aecc02aa96c9975f1fad4f87091b416803990ce
mxxnseat <dumaesnormalno@gmail.com>
2022-06-04 14:20:45 +0300

    feat(camera): add functionality to camera class

--- src/classes/camera.class.ts
+++ src/classes/camera.class.ts
@@ -1,11 +1,47 @@
+import { BootError } from '../common/errors/boot.error';
+
 export class Camera implements HeroOnline.interfaces.IGameCamera {
+  private _position: HeroOnline.interfaces.ClearPosition =
+    null as unknown as HeroOnline.interfaces.ClearPosition;
+  private _isBoot = false;
+
   public get position() {
     return this._position;
   }
 
-  constructor(private _position: HeroOnline.interfaces.IPosition) {}
+  public unboot() {
+    if (!this._isBoot) {
+      throw new BootError(
+        'Cannot unboot camera, because it already not booted',
+      );
+    }
 
-  move(to: HeroOnline.interfaces.IPosition): void {
+    this._position = null as unknown as HeroOnline.interfaces.ClearPosition;
+    this._isBoot = false;
+  }
+
+  public boot(playerPosition: HeroOnline.interfaces.IPosition) {
+    if (this._isBoot) {
+      throw new BootError('Cannot boot camera twice');
+    }
+
+    const cameraPositionX = playerPosition.x / 2;
+    const cameraPositionY = playerPosition.y / 2;
+
+    this._position = {
+      x: cameraPositionX,
+      y: cameraPositionY,
+    };
+
+    this._isBoot = true;
+
+    return void 0;
+  }
+
+  public move(to: HeroOnline.interfaces.IPosition): void {
+    this._position.x += to.x;
+    this._position.y += to.y;
+
     return void 0;
   }
 }
